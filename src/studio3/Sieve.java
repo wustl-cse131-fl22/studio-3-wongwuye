9dc1deebd8748d92770ca26c6772bc1bcecfbcab
aaron <aaron@DESKTOP-L9VE9O6.dhcp.wustl.edu>
2022-09-21 16:33:25 -0500

    gj boys

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,30 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("what is n?");
+		int n = in.nextInt();
+		boolean[] array = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = true;
+		}
+		for (int i = 2; i < Math.sqrt(n); i++){
+			if (array[i] == true) {
+				for (int j = i*i; j < n; j += i) {
+					array[j] = false;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (array[i] == true) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+
+}
